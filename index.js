f539357f0cfb08efe6fc291b8175a00750673fbc
harsh maurya <harshmaurya1152001@gmail.com>
2023-07-21 18:31:02 +0530

    first commit

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.use(passport.session())
 
 const PORT = 5000 || process.env.PORT
 const databaseName = 'RideX';
-const mongoURI = `mongodb+srv://mauryaharshpriya:m09Z1IW69fimJMXS@cluster0.eiabszr.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
-//const mongoURI = `mongodb+srv://mauryaharshpriya:m09Z1IW69fimJMXS@cluster0.eiabszr.mongodb.net/?retryWrites=true&w=majority`
+const mongoURI =  process.env.MONGO_URI;
+
 mongoose.connect(String(mongoURI), {
     useNewUrlParser:true,
     useUnifiedTopology:true
